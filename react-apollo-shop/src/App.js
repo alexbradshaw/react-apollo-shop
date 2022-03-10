81ae827b8901695dd75436b77d7d655a6d60d831
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2022-03-10 00:37:47 -0500

    created basic settings page functionality

--- react-apollo-shop/src/App.js
+++ react-apollo-shop/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Settings from './components/Settings' 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Settings/> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
