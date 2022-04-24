d49a3b8cb8ca41d534a95842bd96bc309311eef2
Sam Sweigart <samuel.sweigart@gmail.com>
2022-04-24 18:39:02 -0400

    added useEffect hook to Carousel component | image cycles ever 3 seconds | animations next

--- react-apollo-shop/src/components/Carousel/Carousel.jsx
+++ react-apollo-shop/src/components/Carousel/Carousel.jsx
@@ -1,21 +1,35 @@
 import './Carousel.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { carouselImgs } from './Images';
 
 const Carousel = () => {
 
-    const [ imgIndex, setImgIndex ] = useState(0);
+    let [ imgIndex, setImgIndex ] = useState(0);
+
+     
+
+        useEffect(()=>{
+            const interval = setInterval(() => {
+            imgIndex < 3 ? setImgIndex(imgIndex + 1) : setImgIndex(0)
+        }, 3000)
+            return () => clearInterval(interval);
+        }, [imgIndex]); 
 
     return ( 
         <div className='carousel'>
-            <img src={`../images/${carouselImgs[imgIndex].path}`} alt="Random photo carousel of random user listings" />
+            <img 
+                src={`../images/${carouselImgs[imgIndex].path}`} 
+                alt="Random photo carousel of random user listings" 
+            />
+            
             {imgIndex > 0 && <button 
                 className='left'
                 onClick={() => setImgIndex(imgIndex - 1)}
             >
                 left 
             </button>}
+
             {imgIndex < carouselImgs.length - 1 && <button 
                 className='right'
                 onClick={() => setImgIndex(imgIndex + 1)}
