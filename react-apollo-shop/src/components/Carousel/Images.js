9fa7840a66383aedc386cf9d2e1a7b4cdc4d87e5
Sam Sweigart <samuel.sweigart@gmail.com>
2022-04-25 11:13:55 -0400

    removed hardcoded values from carousel to scale component to work with any size image array | added curried func to remove arrow functions from jsx

--- react-apollo-shop/src/components/Carousel/Images.js
+++ react-apollo-shop/src/components/Carousel/Images.js
@@ -2,5 +2,6 @@ export const carouselImgs = [
     {id: 1, path: 'img1.jpg'},
     {id: 2, path: 'img2.jpg'},
     {id: 3, path: 'img3.jpg'},
-    {id: 4, path: 'img4.jpg'}
+    {id: 4, path: 'img4.jpg'},
+    {id: 5, path: 'img5.jpg'}
 ]
