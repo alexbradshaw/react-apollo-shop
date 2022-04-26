b2f3edc88da417c3ae2bf25f7b7f7a464480180f
Sam Sweigart <samuel.sweigart@gmail.com>
2022-04-26 12:01:00 -0400

    added dropDown modal | added links for settings and logout

---
+++ react-apollo-shop/src/components/ModalDropDown/ModalDropDown.jsx
@@ -0,0 +1,39 @@
+import './ModalDropDown.css';
+import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+
+
+const ModalDropDown = () => {
+
+    const dropDownVariants = {
+        hidden: {
+           opacity: 0,
+           scale: 0,
+           translateX: 60,
+           translateY: -60,
+        },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            translateX: 0,
+            translateY: 0,
+            transition: {
+                duration: 0
+            }
+        },
+    }
+
+    return ( 
+            <motion.div className='drop-down'
+                variants={dropDownVariants}
+                initial='hidden'
+                animate='visible'
+            >
+                <Link to={'/settings'} className='settings'> Settings </Link>
+                <Link to={'/'} className='logout'> Logout </Link>
+            </motion.div>
+     );
+}
+ 
+export default ModalDropDown;
