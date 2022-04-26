75dbb14c590ef2df6266159aaebef1106a62f284
Sam Sweigart <samuel.sweigart@gmail.com>
2022-04-26 09:17:55 -0400

    added conditional signIn/user profile avatar

--- react-apollo-shop/src/components/Navbar/Navbar.jsx
+++ react-apollo-shop/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,25 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 //* Images
 import burgerMenuDark from './imgs/burger_menu_dark.png';
 import burgerMenuLight from './imgs/burger_menu_light.png';
 import shoppingCartDark from './imgs/shopping_cart_dark.png';
 import shoppingCartLight from './imgs/shopping_cart_light.png';
+import userProfileDark from './imgs/user_profile_dark.png';
+import userProfileLight from './imgs/user_profile_light.png';
 import search from './imgs/search.png';
 
+
 const Navbar = ({ theme, setShowCategories, setShowSignIn, setShowCart }) => {
+
+
+    const [ loggedIn, setLoggedIn ] = useState(true);
+
+    const handleIcons = (dark, light) => {
+        return theme === 'light' ? dark : light;
+    }
 
     return (
         
@@ -17,7 +28,7 @@ const Navbar = ({ theme, setShowCategories, setShowSignIn, setShowCart }) => {
             <h4 className='logo'> <Link to={'/'}> Trasher 🤘 </Link> </h4>
             <img className='burger-menu'
                 /* conditionally renders dark or light burger menu icon */
-                src={theme === 'light' ? burgerMenuDark : burgerMenuLight}
+                src={handleIcons(burgerMenuDark, burgerMenuLight)}
                 onClick={() => setShowCategories(true)}
             />
             <input className='search-bar'
@@ -28,17 +39,26 @@ const Navbar = ({ theme, setShowCategories, setShowSignIn, setShowCart }) => {
             <img className='search-icon'
                 src={search} 
             />
-            <a href='#' 
-                className='sign-in' 
-                onClick={() => setShowSignIn(true)}
-                > 
-                Sign in 
-            </a>
+
+            { loggedIn ? (
+                <img
+                    className='user-profile'
+                    src={handleIcons(userProfileDark, userProfileLight)}
+                />
+            ) : (
+                <a href='#' 
+                    className='sign-in' 
+                    onClick={() => setShowSignIn(true)}
+                    > 
+                    Sign in 
+                </a>
+            )}
             <img className='shopping-cart'
                 onClick={() => setShowCart(true)}
                 /* conditionally renders dark or light cart icon */
-                src={theme === 'light' ? shoppingCartDark : shoppingCartLight}
+                src={handleIcons(shoppingCartDark, shoppingCartLight)}
             />
+            
 
         </nav>
     )
